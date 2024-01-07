5ed71f4fb4fc21dc28b3ec05b7b42d629dc50099
Abdul <aabdulmajeed.isa@gmail.com>
2024-01-07 15:33:48 +0100

    done task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask API"""
+"""Module for the Flask API"""
 import os
 from flask import Flask, jsonify
 from models import storage
@@ -7,17 +7,18 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close(x):
+def close(_):
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found_error(e):
     return jsonify({"error": "Not found"}), 404
+
 
 if os.getenv("HBNB_API_HOST"):
     host = os.getenv("HBNB_API_HOST")
@@ -28,6 +29,7 @@ if os.getenv("HBNB_API_PORT"):
     port = int(os.getenv("HBNB_API_PORT"))
 else:
     port = 5000
+
 
 if __name__ == "__main__":
     app.run(host=host, port=port, threaded=True)
