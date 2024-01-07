b5ebf712ba65c1cdf436c14105b27270d12e7963
Abdul <aabdulmajeed.isa@gmail.com>
2024-01-07 13:47:24 +0100

    add jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """define routes of blueprint"""
 from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status', methods=["GET"])
+@app_views.route('/status', strict_slashes=False , methods=["GET"])
 def status():
-    return {
+    return jsonify({
         "status": "OK",
-    }
+    })
