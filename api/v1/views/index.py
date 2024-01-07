acf8835ae4fae7b8b191d0ccbb319a8caa76a42b
Abdul <aabdulmajeed.isa@gmail.com>
2024-01-07 17:27:28 +0100

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -19,21 +19,21 @@ def status():
     })
 
 
-@app_views.route("/stats", methods=["GET"])
-def stats():
-    """get count for all models"""
-    amenities = storage.count(Amenity)
-    cities = storage.count(City)
-    places = storage.count(Place)
-    reviews = storage.count(Review)
-    states = storage.count(State)
-    users = storage.count(User)
+# @app_views.route("/stats", methods=["GET"])
+# def stats():
+#     """get count for all models"""
+#     amenities = storage.count(Amenity)
+#     cities = storage.count(City)
+#     places = storage.count(Place)
+#     reviews = storage.count(Review)
+#     states = storage.count(State)
+#     users = storage.count(User)
 
-    return jsonify({
-        "amenities": amenities,
-        "cities": cities,
-        "places": places,
-        "reviews": reviews,
-        "states": states,
-        "users": users,
-    })
+#     return jsonify({
+#         "amenities": amenities,
+#         "cities": cities,
+#         "places": places,
+#         "reviews": reviews,
+#         "states": states,
+#         "users": users,
+#     })
