0616b5c0685d355adc9069c09ac5719019c8f7f8
Abdul <aabdulmajeed.isa@gmail.com>
2024-01-07 17:28:50 +0100

    remove function

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,23 +17,3 @@ def status():
     return jsonify({
         "status": "OK"
     })
-
-
-# @app_views.route("/stats", methods=["GET"])
-# def stats():
-#     """get count for all models"""
-#     amenities = storage.count(Amenity)
-#     cities = storage.count(City)
-#     places = storage.count(Place)
-#     reviews = storage.count(Review)
-#     states = storage.count(State)
-#     users = storage.count(User)
-
-#     return jsonify({
-#         "amenities": amenities,
-#         "cities": cities,
-#         "places": places,
-#         "reviews": reviews,
-#         "states": states,
-#         "users": users,
-#     })
