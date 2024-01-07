866aaa115f014d97099510836c51417d2821aecc
Abdul <aabdulmajeed.isa@gmail.com>
2024-01-07 17:33:09 +0100

    add stats function

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,3 +15,23 @@ from models.user import User
 def status():
     """json status"""
     return jsonify(status='OK')
+
+
+@app_views.route("/stats")
+def stats_count():
+    """get count for all models"""
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+
+    return jsonify({
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users,
+    })
